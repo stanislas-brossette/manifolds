d9d1fbd4b73306e1cb1a818c24fdef67957ca668
stanislas-brossette <stanislas.brossette@gmail.com>
2014-12-12 14:16:07 +0900

    Created a class for problem with automatic map composition

---
+++ include/pgsolver/solver/ProblemOnRepresentationSpace.h
@@ -0,0 +1,37 @@
+#ifndef _PGS_PROBLEM_ON_REPRESENTATION_SPACE_H_
+#define _PGS_PROBLEM_ON_REPRESENTATION_SPACE_H_
+
+#include <iostream>
+#include <Eigen/Core>
+
+#include <pgsolver/manifolds/defs.h>
+#include <pgsolver/solver/Problem.h>
+
+namespace pgs
+{
+  /// \brief This kind of problem has its objective and constraints written on
+  /// the representation space of the manifold and does the compositions with the
+  /// map automatically
+  class ProblemOnRepresentationSpace : public Problem
+  {
+    public:
+      ProblemOnRepresentationSpace(Manifold& M);
+      ProblemOnRepresentationSpace(Manifold& manifold, const Point& x);
+      virtual void getUB(RefVec out) const;
+      virtual void getLB(RefVec out) const;
+
+      virtual void getCstrLB(RefVec out, size_t i) const;
+      virtual void getCstrUB(RefVec out, size_t i) const;
+
+      virtual void evalObj(RefVec out) const;
+      virtual void evalObjGrad(RefVec out) const;
+
+      virtual void evalLinCstr(RefVec out, size_t i) const;
+      virtual void evalLinCstrGrad(RefVec out, size_t i) const;
+
+      virtual void evalNonLinCstr(RefVec out, size_t i) const;
+      virtual void evalNonLinCstrGrad(RefVec out, size_t i) const;
+  };
+}
+
+#endif //_PGS_PROBLEM_ON_REPRESENTATION_SPACE_H_
---
+++ src/solver/ProblemOnRepresentationSpace.cpp
@@ -0,0 +1,49 @@
+#include <pgsolver/solver/ProblemOnRepresentationSpace.h>
+
+namespace pgs
+{
+      
+  ProblemOnRepresentationSpace::ProblemOnRepresentationSpace(Manifold& M)
+    : Problem(M)
+  {
+  }
+  ProblemOnRepresentationSpace::ProblemOnRepresentationSpace(Manifold& M, const Point& x)
+    : Problem(M, x)
+  {
+  }
+
+  void ProblemOnRepresentationSpace::getUB(RefVec) const
+  {
+  }
+  void ProblemOnRepresentationSpace::getLB(RefVec) const
+  {
+  }
+
+  void ProblemOnRepresentationSpace::getCstrLB(RefVec, size_t) const
+  {
+  }
+  void ProblemOnRepresentationSpace::getCstrUB(RefVec, size_t) const
+  {
+  }
+
+  void ProblemOnRepresentationSpace::evalObj(RefVec) const
+  {
+  }
+  void ProblemOnRepresentationSpace::evalObjGrad(RefVec) const
+  {
+  }
+
+  void ProblemOnRepresentationSpace::evalLinCstr(RefVec, size_t) const
+  {
+  }
+  void ProblemOnRepresentationSpace::evalLinCstrGrad(RefVec, size_t) const
+  {
+  }
+
+  void ProblemOnRepresentationSpace::evalNonLinCstr(RefVec, size_t) const
+  {
+  }
+  void ProblemOnRepresentationSpace::evalNonLinCstrGrad(RefVec, size_t) const
+  {
+  }
+}
