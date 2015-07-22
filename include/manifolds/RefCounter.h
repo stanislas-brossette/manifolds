a11bd8fea540ff9b80dbf9a675b38e215b8c6774
Duchemin Grégoire <greg.duchemin@gmail.com>
2015-07-22 14:33:34 +0900

    Changed the RefCounter to be active even in release

--- include/manifolds/RefCounter.h
+++ include/manifolds/RefCounter.h
@@ -37,46 +37,34 @@ namespace mnf
   {
     public:
       RefCounter()
-#ifndef NDEBUG
         :count_(0)
-#endif
       {
       }
 
   RefCounter(const RefCounter&)
-#ifndef NDEBUG
     :count_(0)
-#endif
       {
       }
 
       // In C++ 11, by default, a destructor cannot launch an exception, setting noexcet to false allows it.
       ~RefCounter() NOEXCEPT(false)
       {
-#ifndef NDEBUG
         mnf_assert(count_ == 0 && "You cannot destroy this manifold because some points still depend on it");
-#endif
       }
 
     protected:
       void incrementRefCounter() const
       {
-#ifndef NDEBUG
         count_++;
-#endif
       }
       void decrementRefCounter() const
       {
-#ifndef NDEBUG
         mnf_assert(count_>0 && "You cannot decrement when no point exist");
         count_--;
-#endif
       }
 
     private:
-#ifndef NDEBUG
       mutable int count_;
-#endif
       friend void ConstSubPoint::registerPoint();
       friend void ConstSubPoint::unregisterPoint();
   };
